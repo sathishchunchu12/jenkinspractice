61e673df366391d29840d179b6f6aa1316a591f7
sathishchunchu12 <98147764+sathishchunchu12@users.noreply.github.com>
2022-03-09 11:06:52 +0530

    Update firstproject.sh

--- firstproject.sh
+++ firstproject.sh
@@ -1,3 +1,4 @@
 echo "This is first Automation project"
 echo "This is Poll Scm component for Automation in Jenkins"
 echo "Using Git Webhook for Automation"
+echo "using git webhook for Automation example"
